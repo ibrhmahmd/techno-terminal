6ff2a591721ae21b487dd1752a9c88b3a9f97245
ibrhmahmd <ibrahim.ahmd.net@gmail.com>
2026-04-07 09:49:30 +0200

    avoiding bash variable errors

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 
 export PORT=${PORT:-8000}
 
-echo "Starting gunicorn on port $PORT..."
+echo "Starting gunicorn on port 8000..."
 
 exec gunicorn app.api.main:app \
     -k uvicorn.workers.UvicornWorker \
-    --bind 0.0.0.0:$PORT \
+    --bind 0.0.0.0:8000 \
     --workers 2 \
     --timeout 120
