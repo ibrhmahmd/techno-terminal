24d74147e13111f24346d4c52c9e5a0304c47a76
ibrhmahmd <ibrahim.ahmd.net@gmail.com>
2026-04-22 05:12:46 +0200

    removed streamlit UI

--- app/ui/components/auth_guard.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from app.ui import state
-
-
-def require_auth():
-    if not state.is_authenticated():
-        st.switch_page("pages/login.py")
-        st.stop()
-    return state.get_user()
